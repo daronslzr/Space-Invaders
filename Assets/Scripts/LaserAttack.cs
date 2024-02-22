770a955a91f17413cb90aeae32bb7238091d8c93
daronslzr <74737247+daronslzr@users.noreply.github.com>
2024-02-22 01:33:03 -0700

    Added laser attack

---
+++ Assets/Scripts/LaserAttack.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LaserAttack : MonoBehaviour
+{
+    public static LaserAttack Instance { get; private set; }
+    float positionX;
+    //The starting position in the y-axis is fixed
+    float positionY = -3.6f;
+    float speed = 2.5f;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        positionX = PlayerManager.Instance.rb.position.x;
+        transform.position = new Vector2 (positionX, positionY);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+        if(transform.position.y > 5.25)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -10,6 +10,8 @@ public class PlayerManager : MonoBehaviour
     public float dirX;
     public float movementVelocity = 7f;
 
+    public GameObject laserAttackPrefab;
+
     void Awake()
     {
         Instance = this;
@@ -23,6 +25,11 @@ public class PlayerManager : MonoBehaviour
             dirX = Input.GetAxisRaw("Horizontal");
             //Moves the player in the x-axis
             rb.velocity = new Vector2(dirX * movementVelocity, rb.velocity.y);
+            //Shoots laser when d or up arrow is pressed
+            if (Input.GetKeyDown("w") || Input.GetKeyDown("up"))
+            {
+                GameObject laserAttack = Instantiate(laserAttackPrefab, transform.position, Quaternion.identity);
+            }
         }
     }
 }
